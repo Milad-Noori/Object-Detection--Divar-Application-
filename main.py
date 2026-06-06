6a2672d1215d7d1f34df8ac8c98c0fae89bfd4e3
milad <seyeadmiladnouri@gmail.com>
2026-06-06 22:55:59 +0330

    library

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import pandas as pd
+import os
+import warnings
+
+from sklearn.metrics import confusion_matrix
+
+warnings.filterwarnings("ignore")
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from keras import layers, models
+
+import arabic_reshaper
+from bidi.algorithm import get_display
+
+
 
 images = []
